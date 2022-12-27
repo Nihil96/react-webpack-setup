c368901711d7ab0679955f1cfe4fe8a8d8210ae2
hristijankostadinoski <hristijan@2coders.com>
2022-12-27 18:13:01 +0100

    chore: Improve Lighthouse CI feedback by enabling warning and error levels for certain categories

--- lighthouserc.js
+++ lighthouserc.js
@@ -10,6 +10,10 @@ module.exports = {
       preset: "lighthouse:no-pwa",
       // skip the assertions and fix them later
       assertions: {
+        "categories:performance": ["warn", { minScore: 0.9 }],
+        "categories:accessibility": ["warn", { minScore: 0.9 }],
+        "categories:best-practices": ["warn", { minScore: 0.8 }],
+        "categories:seo": ["error", { minScore: 1 }],
         "csp-xss": "warn",
         "render-blocking-resources": "warn",
         "unused-javascript": "off",
