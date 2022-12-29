342eb2dbe6dece2411088e634912a2dd8dfd214a
hristijankostadinoski <hristijan@2coders.com>
2022-12-29 21:36:37 +0100

    chore: Add generate-react-cli for faster component generation

---
+++ src/assets/react-cli/templates/default/TemplateName.tsx
@@ -0,0 +1,10 @@
+import type { FC } from "react"
+import "./TemplateName.scss"
+
+interface TemplateNameProps {}
+
+const TemplateName: FC<TemplateNameProps> = () => {
+  return <div className='TemplateName'>TemplateName</div>
+}
+
+export { TemplateName }
